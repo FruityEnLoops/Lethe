964b544d32e26f5685c8a71e1ed4aaa74b5e808d
Loïc Demay <fruityenloops@gmail.com>
2021-04-03 12:13:23 +0200

    Filtered more accurately actions by user

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ var Twit = require("twit");
 var T = new Twit(twitconfig);
 
 T.stream("statuses/filter", { follow: config.users }).on("tweet", function (tweet) {
-    console.log(`New tweet by ${tweet.user.name}!`);
-    fetch(config.webhook_url, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: tweet.user.name,
-            avatar_url: tweet.user.profile_image_url_https,
-            content: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-        }),
-    }).then(() => console.log(`Received tweet and posted message in ${Date.now() - tweet.timestamp_ms}ms.`));
+    if(config.users.includes(tweet.user.id_str)){
+        console.log(`New tweet by ${tweet.user.name}!`);
+        fetch(config.webhook_url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: tweet.user.name,
+                avatar_url: tweet.user.profile_image_url_https,
+                content: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+            }),
+        }).then(() => console.log(`Received tweet and posted message in ${Date.now() - tweet.timestamp_ms}ms.`));
+    }
 });
